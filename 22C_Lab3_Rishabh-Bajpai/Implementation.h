33a4e5b87cb27dbd20d1ce17b20904ca5a6acf3e
Rishabh Bajpai <rishabh.bajpai@outlook.com>
2014-11-12 08:41:15 -0800

    Added some implementation changes having an error with building the tree

--- 22C_Lab3_Rishabh-Bajpai/Implementation.h
+++ 22C_Lab3_Rishabh-Bajpai/Implementation.h
@@ -8,18 +8,21 @@
 #include <vector>
 
 
+void runInput(std::string inputFileName, std::vector<Person>& people);
+void buildTree(std::vector<Person> people, BST& tree);
 
-void runInput(std::string inputFileName, std::vector<Person> people);
 
-void runInput(std::string inputFileName, std::vector<Person> people)
+void runInput(std::string inputFileName, std::vector<Person>& people)
 {
-	int inDay, inMonth, inYear;
+	int inDay, inMonth, inYear,i=0;
+	Birthday inBday;
 	char separator;
 	std::string inName;
 	std::ifstream myfile;
 	myfile.open(inputFileName);
-	while ()
+	while (myfile.good())
 	{
+		
 		myfile >> inDay;
 
 		myfile >> separator;
@@ -36,9 +39,28 @@ void runInput(std::string inputFileName, std::vector<Person> people)
 
 		// Ignore the newline, as it is still in the buffer.
 		myfile.ignore(10000, '\n');
-
+		inBday.day = inDay;
+		inBday.month = inMonth;
+		inBday.year = inYear;
 		// Process or store values.
-
+		people[i].insertValues(inBday, inName);
+		i += 1;
 	}
 	myfile.close();
+}
+
+
+void buildTree(std::vector<Person> people, BST& tree)
+{
+	int j = people.size;
+	if (j == 0)
+	{
+		std::cout << "Your list of people is empty" << std::endl
+			<< "Was your input file formatted correctly?" << std::endl
+			<< "Check to make sure it is in 'DD,MM,YYYY' format" << std::endl;
+	}
+	for (int i = 0; i < j; i++)
+	{
+		tree.insert(people[i]);
+	}
 }
