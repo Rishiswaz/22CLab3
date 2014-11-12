437c8bbbe97c3caedb827722722deec102ad0b39
Rishabh Bajpai <rishabh.bajpai@outlook.com>
2014-11-11 22:34:47 -0800

    Initial file commits

---
+++ 22C_Lab3_Rishabh-Bajpai/22C_Lab3_Rishabh-Bajpai.cpp
@@ -0,0 +1,11 @@
+// 22C_Lab3_Rishabh-Bajpai.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "person.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
---
+++ 22C_Lab3_Rishabh-Bajpai/person.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+struct Birthday
+{
+	//structure for holding the values for a person's birthday
+	int day, month, year;
+};
+
+class Person
+{
+public:
+	Person();
+	~Person();
+	void instertValues(Birthday bdayIn, std::string nameIn);
+	void output_PO();
+	void output_BF();
+private:
+	Birthday dOB; //date of birth
+	std::string name; //name of individual
+};
+
+Person::Person()
+{
+
+}
+
+Person::~Person()
+{
+}
+
+void Person::output_PO()
+{
+	std::ofstream myfile;
+	myfile.open("post-order_output.txt");
+	myfile << name << " " << dOB.day << " " << dOB.month << " " << dOB.year;
+	myfile.close();
+}
+void Person::output_BF()
+{
+	std::ofstream myfile;
+	myfile.open("breadth-first_output.txt");
+	myfile << name << " " << dOB.day << " " << dOB.month << " " << dOB.year;
+	myfile.close();
+}
+
+void Person::instertValues(Birthday bdayIn, std::string nameIn)
+{
+	dOB = bdayIn;
+	name = nameIn;
+}
